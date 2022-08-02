9d90ca40682fc03a83c2f84e2cc7100ba19948c2
gusscamargo <guga.power@hotmail.com>
2022-08-02 17:22:06 -0300

    Adicionando main

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <array>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <functional>
+#include <tuple>
+#include <numeric>
+#include <set>
+#include <bitset>
+#include <cmath>
+
+#define F(x) for(int i = 0; i < x; i++)
+#define pb push_back
+#define Feach(x) for(auto item : x)
+#define mp make_pair
+#define mt make_tuple
+
+using namespace std;
+const double PI = 3.14159265358979323846;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long long int lli;
+typedef long double ld;
+typedef unsigned int uint;
+typedef vector<int> vi;
+
+int main(){
+    ios::sync_with_stdio(0);
+    std::cin.tie(0);
+
+
+    return 0;
+}
