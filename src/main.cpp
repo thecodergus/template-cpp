6291b2d4e6b378ddcba00a518fba3659090df29c
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-09-05 14:53:43 -0300

    Tornando mais neutro o template

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,6 @@
 #include <iostream>
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <array>
-#include <queue>
-#include <utility>
-#include <algorithm>
-#include <map>
-#include <functional>
-#include <tuple>
-#include <numeric>
-#include <set>
-#include <bitset>
-#include <cmath>
-#include <cassert>
-
-#define F(x) for(int i = 0; i < x; i++)
-#define Feach(x) for(auto item : x)
-#define Fiter(t, x) for(t::iterator i = x.begin(); i < x.end(); i++)
-#define pb push_back
-#define mp make_pair
-#define mt make_tuple
-#define fi first
-#define se second
-
-using namespace std;
-const double PI = 3.14159265358979323846;
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long long int lli;
-typedef long double ld;
-typedef unsigned int uint;
-typedef vector<int> vi;
-typedef vector<bool> vb;
-typedef vector<string> vs;
-typedef pair<int, int> ii;
 
 int main(){
-    ios::sync_with_stdio(0);
-    std::cin.tie(0);
-
+    std::cout << "Ola" << std:endl;
     return 0;
 }
